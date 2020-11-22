36c1b34597af6cc1559dea8e8ecf1264e57caf9d
zhengjialux <15879398507@163.com>
2020-11-22 22:33:20 +0800

    项目初始化

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const birds = require('./birds')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/birds', birds)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
